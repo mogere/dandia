3cbbf3c99a729f52513fa6748746b0dc36a6c5c8
kolumr <kolumronnie@gmail.com>
2019-11-14 18:20:45 +0300

    Log in validation

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var users = [{
+  username : "Kolum",
+  email:"kolumronnie@gmail.com",
+  password : "12345"
+},
+{
+  username : "Tichi",
+  email:"tichi@gmail.com",
+  password : "54321"
+}
+]
+function getInfo(){
+  
+  var email = document.getElementById("defaultForm-email").value;
+  
+  var password = document.getElementById("defaultForm-pass").value;
+  // var username = document.getElementById("username").value;
+  // alert("Your username is " + username + " and your email is " + email + " and Password is " + password)
+  for(i=0;i<users.length;i++){
+    
+    if(email == users[i].email && password == users[i].password){
+      alert(users[i].username + " is logged in");
+      $(location).attr('href',`book.html`);
+      return
+    }
+    
+  }
+  alert("Wrong username or password");
+}
+function User(username,email,password){
+  this.username = username;
+  this.email = email;
+  this.password = password;
+
+}
+function signUp(){
+  var email = document.getElementById("orangeForm-email").value;
+  var password = document.getElementById("orangeForm-pass").value;
+  var username = document.getElementById("orangeForm-name").value;
+  // newuser = new User(username,email,password)
+  // alert(email);
+  users.push(new User(username,email,password));
+  // alert(JSON.stringify(users));
+  console.log(users);
+}
