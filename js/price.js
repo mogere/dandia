f11e2aea19c16864375f5d6ed44eaf3ccac866aa
mogere <pmnyatichi@gmail.com>
2019-11-15 07:45:41 +0300

    fix modal app

--- js/price.js
+++ js/price.js
@@ -12,13 +12,12 @@ function caculatePrice(passengers, locationScale){
     
 }
 function getData(){
-    $("#reserve").click(function () {
-        $('#booked').modal('show')
-})
+    $("#reserve").click(
+        $('#booked').modal('show'))
 }
 
 function getDriver(){
-    $("#reserve1").click(function () {
+    $("#reserve1").click(
         $('#booked1').modal('show')
-})
+)
 }
